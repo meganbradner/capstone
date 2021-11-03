6ea028d17a963504f07a3a973c929ac24879d542
meganbradner <meganrbradner@gmail.com>
2021-11-03 16:14:14 -0700

    made changes

--- capstone_app/models.py
+++ capstone_app/models.py
@@ -9,6 +9,8 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50)
     bio = models.CharField(max_length=200)
     icon = models.ImageField(upload_to="images/")
+    slug = AutoSlugField(populate_from='username', null=True)
+
 
     def __str__(self):
         return self.username
