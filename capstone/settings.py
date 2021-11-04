46ff6279b2646c1b979e696c81ef1e526cf6354c
meganbradner <meganrbradner@gmail.com>
2021-11-04 14:32:01 -0700

    shuffled settings order

--- capstone/settings.py
+++ capstone/settings.py
@@ -103,12 +103,12 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -157,6 +157,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import django_on_heroku
+django_on_heroku.settings(locals())
+
 from hidden import key, secret
 
 CLOUDINARY_STORAGE = {
@@ -166,6 +169,3 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-import django_on_heroku
-django_on_heroku.settings(locals())
